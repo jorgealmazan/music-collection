6f4a94d3b5a798639660f57eb38bc73957d0bb82
Jorge Almazan <jorge.almazan87@live.com>
2019-06-16 12:36:15 -0500

    Small fixed artist serializer

--- mirage/serializers/artist.js
+++ mirage/serializers/artist.js
@@ -1,11 +1,5 @@
 import ApplicationSerializer from './application';
 
 export default ApplicationSerializer.extend({
-  links(artist) {
-    return {
-      albums: {
-        related: `/artists/${artist.id}/albums`
-      }
-    };
-  }
+  include: ['albums']
 });
